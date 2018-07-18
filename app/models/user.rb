03feb619d144e1865bf43fd197f896e1db55bc8b
George Sykes <georgesykes86@gmail.com>
2018-07-18 20:56:16 +0100

    Add unique email validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :articles, dependent: :delete_all
   validates :password, presence: true, length: {minimum: 8, maximum: 15}
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP },
-    length: {maximum: 100}
+    length: {maximum: 100}, uniqueness: true
 end
