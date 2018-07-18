6e7908959398d097ff398ece6b21d9241e8f4f01
George Sykes <georgesykes86@gmail.com>
2018-07-18 20:48:28 +0100

    Adds test for invalid emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :delete_all
-  validates :password, presence: true, length: {minimum: 8}
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: {minimum: 8, maximum: 15}
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP },
+    length: {maximum: 100}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,22 +11,45 @@ class UserTest < ActiveSupport::TestCase
 
   test "should not be valid if email not present" do
     @user.email = nil
-    assert !@user.valid?
+    assert_not @user.valid?
   end
 
   test "should not be valid if no password" do
     @user.password = nil
-    assert !@user.valid?
+    assert_not @user.valid?
   end
 
   test "should not be valid if not a proper email" do
-    @user.email = "georgesykes"
-    assert !@user.valid?
+    invalid_addresses = %w[user@example,com USER@something.@ A_USER.NEW@foo.bar.
+                           first@user_cn john@gemr+any.e]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should not be valid"
+    end
   end
 
-  test "should not be valid if not the correct length password" do
+  test "should be valid if using a proper email" do
+    valid_addresses = %w[user@example.com USER@something.com A_USER-NEW@foo.bar.org
+                           first@user.cn john@gemrany.de]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "password should not be too short" do
     @user.password = "pass"
-    assert !@user.valid?
+    assert_not @user.valid?
+  end
+
+  test "password should not be too long" do
+    @user.password = "p" * 16
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 100 + "@example.com"
+    assert_not @user.valid?
   end
 
 end
