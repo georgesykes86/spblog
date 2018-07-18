92316f7021b72fbd9dddda93c2b8f07c46e961c4
George Sykes <georgesykes86@gmail.com>
2018-07-18 20:16:01 +0100

    Add unit test for user model validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def get_season()
+    time = Time.new
+
+    if(time.month >= 3) && (time.month <= 5)
+      "Spring"
+    elsif(time.month > 5) && (time.month <= 8)
+      "Summer"
+    elsif(time.month > 8) && (time.month <= 12)
+      "Autumn"
+    else
+      "Winter"
+    end
+  end
+
+  def get_random_number(max_value)
+    rand(max_value)
+  end
+
+  def get_random_welcome()
+    opening = ["What a beautiful day. ", "Welcome to our site. ", "Thanks for stopping by. "]
+    middle = ["We hope you find our site useful. ", "If there is anything you need just ask. ", "Take a look. "]
+    ending = ["Happy hunting!", "Good luck!", "Bon chance!"]
+    opening[get_random_number(opening.length)] +
+    middle[get_random_number(middle.length)] +
+    ending[get_random_number(ending.length)]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "george@email.com", password: "georgeyporgey")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should not be valid if email not present" do
+    @user.email = nil
+    assert !@user.valid?
+  end
+
+  test "should not be valid if no password" do
+    @user.password = nil
+    assert !@user.valid?
+  end
+
+  test "should not be valid if not a proper email" do
+    @user.email = "georgesykes"
+    assert !@user.valid?
+  end
+
+  test "should not be valid if not the correct length password" do
+    @user.password = "pass"
+    assert !@user.valid?
+  end
+
 end
