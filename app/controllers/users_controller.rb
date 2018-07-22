2c8fcdc530d285f7ade36546e4c085525cc82ff6
George Sykes <georgesykes86@gmail.com>
2018-07-22 18:59:33 +0100

    Add access control to edit and update fo profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -71,5 +72,12 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:password, :email, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "please log in"
+        redirect_to login_url
+      end 
     end
 end
